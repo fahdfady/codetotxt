3354a31f3223c27deca7aa31db85e63f48bcb526
fahdfady <fahd.fady212@gmail.com>
2025-02-22 20:14:45 +0200

    project setup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { promises as fs } from 'fs';
+
+async function main() {
+    const data = await fs.readFile('data.txt', 'utf-8');
+    console.log(data);
+}
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export type MergeOptions = {
+    inputDir: string;
+    outputFile: string;
+    ignoreList: string[];
+}
